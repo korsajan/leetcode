f4d1703d5afb1ab822e63c41a79550a4ea33b2f4
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2023-12-16 10:42:55 +0400

    linkedlist: add tow number

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -2,6 +2,8 @@ package linkedlist
 
 import (
 	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -29,4 +31,50 @@ func TestReverseList(t *testing.T) {
 			t.Errorf("test %d failed ex: %v want: %v", i+1, test.out, l)
 		}
 	}
+}
+
+func TestAddTwoNumbers(t *testing.T) {
+	testTable := []struct {
+		l1, l2, out *ListNode
+	}{
+		{
+			l1:  SliceToList(2, 4, 3),
+			l2:  SliceToList(5, 6, 4),
+			out: SliceToList(7, 0, 8),
+		},
+		{
+			l1:  SliceToList(0),
+			l2:  SliceToList(0),
+			out: SliceToList(0),
+		},
+		{
+			l1:  SliceToList(9, 9, 9, 9, 9, 9, 9),
+			l2:  SliceToList(9, 9, 9, 9),
+			out: SliceToList(8, 9, 9, 9, 0, 0, 0, 1),
+		},
+	}
+	for i, test := range testTable {
+		l := addTwoNumbers(test.l1, test.l2)
+		if !reflect.DeepEqual(l, test.out) {
+			t.Errorf("test %d failed ex: %v want: %v", i+1,
+				ToString(test.out),
+				ToString(l))
+		}
+	}
+}
+
+func ToString(l *ListNode) string {
+	var b strings.Builder
+	b.WriteRune('{')
+	n := l
+	for n != nil {
+		s := strconv.Itoa(n.Val)
+		b.WriteString(s)
+		if n.Next != nil {
+			b.WriteString("->")
+		}
+		n = n.Next
+	}
+	b.WriteRune('}')
+	return b.String()
 }
