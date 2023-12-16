195dff553e25fc4ef6780c6264772dc7fb7fde6e
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2023-12-16 10:42:55 +0400

    linkedlist: add tow number

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -29,6 +29,49 @@ func HasCycle(head *ListNode) bool {
 	return false
 }
 
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var (
+		res  *ListNode
+		prev *ListNode
+		node *ListNode
+	)
+
+	var over bool
+	for l1 != nil || l2 != nil {
+		sum := 0
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		if over {
+			sum++
+		}
+
+		if sum >= 10 {
+			over = true
+		} else {
+			over = false
+		}
+
+		node = &ListNode{Val: sum % 10, Next: nil}
+		if res == nil {
+			res = node
+		} else {
+			prev.Next = node
+		}
+		prev = node
+	}
+	if over {
+		node.Next = &ListNode{Val: 1, Next: nil}
+	}
+	return res
+}
+
 func SliceToList(arr ...int) *ListNode {
 	var node *ListNode
 	for i := 0; i < len(arr); i++ {
